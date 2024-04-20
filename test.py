297b0b14b395e8dfa7d96875b861a38ea83f1e97
zahar-kohut-ucu <kohut.pn@ucu.edu.ua>
2024-04-20 19:30:58 +0300

    quite working version, no evaluation

--- test.py
+++ test.py
@@ -1,19 +1,27 @@
 import numpy as np
 import mixer
+import preprocessing
 import librosa
 import soundfile as sf
+import ica
 
-source1, sample_rate1 = librosa.load("./input_data/mao.mp3")
-source2, sample_rate2 = librosa.load("./input_data/erika.mp3")
+source1, sample_rate2 = librosa.load("./input_data/erika.mp3")
+source2, sample_rate1 = librosa.load("./input_data/mao.mp3")
+source3, sample_rate1 = librosa.load("./input_data/jojo.mp3")
 
-myMixer = mixer.Mixer(2, [[4.5, 4.5, 1.0], [6.5, 6.5, 1.0]], [[5.0, 5.0, 5.0], [9.0, 9.0, 6.0]])
-myMixer.create_room(sample_rate1, dimensions=[10.0, 10.0, 10.0], rt = 0.3)
-myMixer.load_sources_from_arrays([source1, source2 * 1.5])
+# myMixer = mixer.Mixer(2, [[4.5, 4.5, 1.0], [6.5, 6.5, 1.0]], [[5.0, 5.0, 5.0], [9.0, 9.0, 6.0]])
+# myMixer.create_room(sample_rate1, dimensions=[10.0, 10.0, 10.0], rt = 0.3)
+# myMixer.load_sources_from_arrays([source1, source2 * 1.5])
 
-results = myMixer.simulate_and_return_recordings()
+# results = myMixer.simulate_and_return_recordings()
 
-audio_matrix = np.array([results[0], results[1]])
+mixed_sources = preprocessing.preprocess_data([source1, source2, source3], [[0.2, 0.4, 0.4], [0.15, 0.7, 0.15], [0.1, 0.2, 0.7]])
+
+EPS = 0.000000000000000000001
+
+sources = ica.ica(mixed_sources, epsilon=EPS)
+
+for i in range(len(sources)):
+    sf.write(f"./output_data/micro{i}.mp3", sources[i], sample_rate1)
 
 
-sf.write("./output_data/micro1.mp3", results[0], sample_rate1)
-sf.write("./output_data/micro2.mp3", results[1], sample_rate2)
